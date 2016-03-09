10c649d12e08b1a6b8343f855845e8ddf650ea29
kmwboston <kmwboston@gmail.com>
2016-03-08 20:52:04 -0500

    added todo's, and couple other things

--- src/squarez.js
+++ src/squarez.js
@@ -13,8 +13,15 @@
         };
         var defaults = {
             width: 300,
-            height: 300
+            height: 300,
+            transitionTime: 1
         };
+
+        //todo: needs auto rotate function, rotate controls, color, opacity
+
+        //todo: make it so that they can put whatever they want on the faces??
+
+        //todo: separate function to standardize gifs/images on faces??
 
         $.fn.squarez = function(options){
             //initialize namespace var
@@ -45,9 +52,13 @@
                 $(cube_wrapper).css({
                     'width': cubeWidth,
                     'height': cubeHeight
-                })
+                });
 
                 $(el).css({
+                    '-webkit-transition': '-webkit-transform '+square.settings.transitionTime+'s',
+                    '-moz-transition': '-moz-transform '+square.settings.transitionTime+'s',
+                    '-o-transition': '-o-transform '+square.settings.transitionTime+'s',
+                    'transition': 'transform '+square.settings.transitionTime+'s',
                     '-webkit-transform-origin':cubeHeight/2+'px '+cubeWidth/2+'px '+(0-cubeHeight/2)+'px',
                     '-moz-transform-origin':cubeHeight/2+'px '+cubeWidth/2+'px '+(0-cubeHeight/2)+'px',
                     '-ms-transform-origin':cubeHeight/2+'px '+cubeWidth/2+'px '+(0-cubeHeight/2)+'px',
@@ -112,90 +123,3 @@
         console.log("Squarez already defined.");
     }
 })(jQuery);
-
-
-
-
-
-
-
-
-
-//var a = ($(".cube-wrapper").outerWidth());
-//var b = ($(".cube-wrapper").outerHeight());
-//
-//$('#cube').css({
-//    '-webkit-transform-origin':cubeHeight/2+'px '+cubeWidth/2+'px '+(0-cubeHeight/2)+'px',
-//    '-moz-transform-origin':cubeHeight/2+'px '+cubeWidth/2+'px '+(0-cubeHeight/2)+'px',
-//    '-ms-transform-origin':cubeHeight/2+'px '+cubeWidth/2+'px '+(0-cubeHeight/2)+'px',
-//    'transform-origin':cubeHeight/2+'px '+cubeWidth/2+'px '+(0-cubeHeight/2)+'px'
-//});
-//
-//$('.front').css({
-//    '-webkit-transform': 'translateZ(' + 0 + 'px)',
-//    '-moz-transform': 'translateZ(' + 0 + 'px)',
-//    'transform': 'translateZ(' + 0 + 'px)',
-//    'border':'1px solid black'
-//});
-//$('.back').css({
-//    '-webkit-transform': 'rotateY('+(0-180)+'deg) translateZ(' + a + 'px)',
-//    '-moz-transform': 'rotateY('+(0-180)+'deg) translateZ(' + a + 'px)',
-//    '-ms-transform': 'rotateY('+(0-180)+'deg) translateZ(' + a + 'px)',
-//    'transform': 'rotateY('+(0-180)+'deg) translateZ(' + a + 'px)',
-//    'border':'1px solid black'
-//});
-//$('.bottom').css({
-//    '-webkit-transform': 'rotateX('+(0-90)+'deg) translateZ('+(cubeWidth/2)+'px) translateY('+(cubeWidth/2)+'px)',
-//    '-moz-transform': 'rotateX('+(0-90)+'deg) translateZ('+(cubeWidth/2)+'px) translateY('+(cubeWidth/2)+'px)',
-//    '-ms-transform': 'rotateX('+(0-90)+'deg) translateZ(' + (cubeWidth/2) + 'px) translateY('+(cubeWidth/2)+'px)',
-//    'transform': 'rotateX('+(0-90)+'deg) translateZ('+(cubeWidth/2)+'px) translateY('+(cubeWidth/2)+'px)',
-//    'border':'1px solid black'
-//});
-//$('.top').css({
-//    '-webkit-transform': 'rotateX('+90+'deg) translateZ(' +cubeHeight/2 + 'px) translateY('+(0-cubeHeight/2)+'px)',
-//    '-moz-transform': 'rotateX('+90+'deg) translateZ(' +cubeHeight/2 + 'px) translateY('+(0-cubeHeight/2)+'px)',
-//    '-ms-transform': 'rotateX('+90+'deg) translateZ(' +cubeHeight/2 + 'px) translateY('+(0-cubeHeight/2)+'px)',
-//    'transform': 'rotateX('+90+'deg) translateZ(' +cubeHeight/2 + 'px) translateY('+(0-cubeHeight/2)+'px)',
-//    'border':'1px solid black'
-//});
-//$('.right').css({
-//    '-webkit-transform': 'rotateY('+90+'deg) translateZ(' +cubeWidth/2 + 'px) translateX('+(cubeWidth/2)+'px)',
-//    '-moz-transform': 'rotateY('+90+'deg) translateZ(' +cubeWidth/2 + 'px) translateX('+(cubeWidth/2)+'px)',
-//    '-ms-transform': 'rotateY('+90+'deg) translateZ(' +cubeWidth/2 + 'px) translateX('+(cubeWidth/2)+'px)',
-//    'transform': 'rotateY('+90+'deg) translateZ(' +cubeWidth/2 + 'px) translateX('+(cubeWidth/2)+'px)',
-//    'border':'1px solid black'
-//});
-//$('.left').css({
-//    '-webkit-transform': 'rotateY('+(0-90)+'deg) translateZ('+ cubeWidth/2 +'px) translateX('+(0-cubeWidth/2)+'px)',
-//    '-moz-transform': 'rotateY('+(0-90)+'deg) translateZ('+ cubeWidth/2 +'px) translateX('+(0-cubeWidth/2)+'px)',
-//    '-ms-transform': 'rotateY('+(0-90)+'deg) translateZ('+ cubeWidth/2 +'px) translateX('+(0-cubeWidth/2)+'px)',
-//    'transform': 'rotateY('+(0-90)+'deg) translateZ('+ cubeWidth/2 +'px) translateX('+(0-cubeWidth/2)+'px)',
-//    'border':'1px solid black'
-//});
-//
-//
-//var ilr = 0;
-//$('.rotate-button-lr').click(function() {
-//    ilr++;
-//    var degrees = ilr * -90;
-//    var rotation = 'rotateY('+degrees+'deg)';
-//
-//    $('#cube').css({
-//        '-webkit-transform': rotation,
-//        '-moz-transform': rotation,
-//        '-ms-transform': rotation
-//    })
-//});
-//
-//var itb = 0;
-//$('.rotate-button-tb').click(function() {
-//    itb++;
-//    var degrees = itb * -90;
-//    var rotation = 'rotateX('+degrees+'deg)';
-//
-//    $('#cube').css({
-//        '-webkit-transform': rotation,
-//        '-moz-transform': rotation,
-//        '-ms-transform': rotation
-//    })
-//});
